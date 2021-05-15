71389b28c4c39be0d60932c7a7c0e656034d9cd1
et77 <81599370+pon56@users.noreply.github.com>
2021-05-15 19:39:39 +0900

    追加機能完成まで

---
+++ index.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  //your script here.
+
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し初期化する
+  const inputText = document.getElementById("addText").value;
+  document.getElementById("addText").value = "";
+
+  // li生成
+  const li = document.createElement("li");
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // pタグ生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // divタグにpを設定
+  div.appendChild(p);
+  // liタグに上のdivを設定
+  li.appendChild(div);
+
+  console.log(li);
+  // 未完了のリストに上のli を追加
+
+  $('#incompleteList').append(li);
+};
+
+document
+  .getElementById("addButton")
+  .addEventListener("click", () => onClickAdd());
+  
+})
